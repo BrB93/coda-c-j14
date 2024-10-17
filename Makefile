6998153fea19d1f0436f144618ee26ffd0a3581d
Brb93 <bryanbrik@gmail.com>
2024-10-17 12:22:10 +0200

    ex4

--- Makefile
+++ Makefile
@@ -1,13 +1,14 @@
 # Makefile
 
-NAME = ex1
+NAME = ex-4
 
-SRCS = main.c 
+SRCS = main.c
 
 all :  $(NAME)
 
 $(NAME): $(SRCS)
-	gcc $(SRCS) -o $(NAME) `sdl2-config --cflags --libs`
+	gcc $(SRCS) -o $(NAME) `sdl2-config --cflags --libs` -lSDL2_image
+    ##MacOS gcc $(SRCS) -o $(NAME) -I/opt/homebrew/include -L/opt/homebrew/lib -lSDL2 -lSDL2_image
 
 fclean:
 	rm -f $(NAME)
--- main.c
+++ main.c
@@ -1,113 +1,196 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-#include <stdio.h>
+
+char * get_file(FILE * fp)
+{
+    char * content;
+    char buffer[10];
+
+    content = malloc(sizeof(char));
+    content[0] = '\0';
+
+    while(fgets(buffer, 10, fp) != NULL)
+    {
+        content = realloc(content, (strlen(buffer) * sizeof(char)) + strlen(content) * sizeof(char) + 1);
+        content = strcat(content, buffer);
+    }
+
+    return content;
+}
+
+int count_lines(char *str)
+{
+    int i = 0;
+    int lines = 0;
+
+    while(str[i] != '\0')
+    {
+        if(str[i] == '\n')
+        {
+            lines++;
+        }
+
+        i++;
+    }
+
+    return lines;
+}
+
+void display_tab(char **tab, int lines)
+{
+    int i = 0;
+
+    while(i < lines)
+    {
+        printf("%s\n", tab[i]);
+        i++;
+    }
+}
 
 int main()
 {
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) < 0) {
+    FILE *fp = fopen("map.txt", "r");
+    char * file_content;
+    char **tab;
+    int lines;
+    char * line;
+    int i = 0;
+    int j = 0;
+
+    if (fp == NULL)
+    {
+        printf("Le fichier map.txt n'a pas pu être ouvert\n");
+        return EXIT_FAILURE;
+    }
+
+    file_content = get_file(fp);
+
+    fclose(fp);
+    
+    lines = count_lines(file_content);
+
+    tab = malloc(lines * sizeof(*tab));
+
+    line = strtok(file_content, "\n");
+    while((line != NULL) && (i < lines))
+    {
+        tab[i] = malloc(strlen(line) * sizeof(char));
+        tab[i] = strcpy(tab[i], line);
+        i++;
+
+        line = strtok(NULL, "\n");
+    }
+
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) < 0)
+    {
         printf("Error SDL2 Init : %s\n", SDL_GetError());
         return 1;
     }
 
-    SDL_Window* window = SDL_CreateWindow("ex1", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 160, 160, SDL_WINDOW_OPENGL);
-    if (window == NULL) {
-        printf("Error Window Creation: %s\n", SDL_GetError());
-        SDL_Quit();
-        return 3;
+    if (IMG_Init(IMG_INIT_PNG) == 0) 
+    {
+        printf("Erreur d'initialisation de la SDL_Image\n");
+        return 1;
     }
 
-    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
-        printf("Erreur d'initialisation de la SDL_Image: %s\n", IMG_GetError());
-        SDL_DestroyWindow(window);
-        SDL_Quit();
+    SDL_Window* window = SDL_CreateWindow("Exercice 4", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 320, 160, SDL_WINDOW_OPENGL);
+
+    if (window == NULL) 
+    {
+        printf("Error while creating a window : %s\n", SDL_GetError());
         return 1;
     }
 
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (renderer == NULL) {
-        printf("Erreur à la création du renderer: %s\n", SDL_GetError());
-        IMG_Quit();
-        SDL_DestroyWindow(window);
-        SDL_Quit();
+    if (renderer == NULL) 
+    {
+        printf("Erreur à la création du renderer\n");
         return 1;
     }
 
-    SDL_Surface* surface = IMG_Load("bottle.png");
-    if (surface == NULL) {
-        printf("Error loading image: %s\n", IMG_GetError());
-        SDL_DestroyRenderer(renderer);
-        IMG_Quit();
-        SDL_DestroyWindow(window);
-        SDL_Quit();
+    SDL_Surface* surface = IMG_Load("levels.png");
+    if (surface == NULL) 
+    {
+        printf("Error loading image %s\n", IMG_GetError());
         return 1;
     }
 
     SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-    SDL_FreeSurface(surface);
-
-    if (texture == NULL) {
-        printf("Error creating texture: %s\n", SDL_GetError());
-        SDL_DestroyRenderer(renderer);
-        IMG_Quit();
-        SDL_DestroyWindow(window);
-        SDL_Quit();
+    if (texture == NULL) 
+    {
+        printf("Error creating texture\n");
         return 1;
     }
 
-    SDL_Rect destRect;
-    destRect.w = surface->w;
-    destRect.h = surface->h;
-    destRect.x = (160 - destRect.w) / 2; // Position X centrée
-    destRect.y = (160 - destRect.h) / 2; // Position Y centrée
+    SDL_FreeSurface(surface);   
 
-    int running = 1;
-    SDL_Event event;
-    while (running) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                running = 0;
-            } else if (event.type == SDL_KEYDOWN) {
-                // Déplacer l'image en fonction de la touche pressée
-                if (event.key.keysym.scancode == SDL_SCANCODE_LEFT) {
-                    destRect.x -= 5; // Déplace vers la gauche
-                }
-                if (event.key.keysym.scancode == SDL_SCANCODE_RIGHT) {
-                    destRect.x += 5; // Déplace vers la droite
-                }
-                if (event.key.keysym.scancode == SDL_SCANCODE_UP) {
-                    destRect.y -= 5; // Déplace vers haut
-                }
-                if (event.key.keysym.scancode == SDL_SCANCODE_DOWN) {
-                    destRect.y += 5; // Déplace vers bas
-                }
+    while(1)
+    {
+        SDL_Event e;
+        if (SDL_WaitEvent(&e)) 
+        {
+            if (e.type == SDL_QUIT) 
+            {
+                break;
             }
         }
 
-        // Vérification des limites pour ne pas sortir de la fenêtre
-        if (destRect.x < 0) {
-            destRect.x = 0; // Limite gauche
-        } else if (destRect.x > 160 - destRect.w) {
-            destRect.x = 160 - destRect.w; // Limite droite
-        }
-        if (destRect.y < 0)
+        SDL_RenderClear(renderer);
+        i = 0;
+        while(i < lines)
         {
-            destRect.y = 0;                            //limite hauteur
-        } else if (destRect.y > 160 - destRect.h)
-        {
-            destRect.y = 160 - destRect.h;
+            j = 0;
+            while(j < 20)
+            {
+                if(tab[i][j] == '0')
+                {
+                    SDL_Rect rect = {j * 16, i * 16, 16, 16};
+                    SDL_Rect rect_img = {0,0, 16, 16};
+                    SDL_RenderCopy(renderer, texture, &rect_img, &rect);
+                }
+                else if(tab[i][j] == '1')
+                {
+                    SDL_Rect rect = {j * 16, i * 16, 16, 16};
+                    SDL_Rect rect_img = {16,0, 16, 16};
+                    SDL_RenderCopy(renderer, texture, &rect_img, &rect);
+                }
+                else if(tab[i][j] == '2')
+                {
+                    SDL_Rect rect = {j * 16, i * 16, 16, 16};
+                    SDL_Rect rect_img = {32,0, 16, 16};
+                    SDL_RenderCopy(renderer, texture, &rect_img, &rect);
+                }
+                else if(tab[i][j] == '3')
+                {
+                    SDL_Rect rect = {j * 16, i * 16, 16, 16};
+                    SDL_Rect rect_img = {48,0, 16, 16};
+                    SDL_RenderCopy(renderer, texture, &rect_img, &rect);
+                }
+                else if(tab[i][j] == '4')
+                {
+                    SDL_Rect rect = {j * 16, i * 16, 16, 16};
+                    SDL_Rect rect_img = {64,0, 16, 16};
+                    SDL_RenderCopy(renderer, texture, &rect_img, &rect);
+                }
+
+                j++;
+            }
+
+            i++;
         }
 
-        SDL_SetRenderDrawColor(renderer, 156, 150, 150, 255);
-        SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, texture, NULL, &destRect);
         SDL_RenderPresent(renderer);
     }
 
-    SDL_DestroyTexture(texture);
-    SDL_DestroyRenderer(renderer);
-    IMG_Quit();
-    SDL_DestroyWindow(window);
+
     SDL_Quit();
 
-    return 0;
+    for (i = 0; i < lines; i++) {
+        free(tab[i]);
+    }
+    free(tab);
+    free(file_content);
+    return 0; 
 }
